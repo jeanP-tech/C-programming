761cb3d7fc0021a43a680bef9ed06f8a6e44aa01
jeanP-tech <45560971+jeanP-tech@users.noreply.github.com>
2019-03-15 16:27:09 +0900

    Update 19. calculator exercise.c

--- 19. calculator exercise.c
+++ 19. calculator exercise.c
@@ -1,41 +1,41 @@
 // 메인함수의 인자를 이용한 계산기
-// 미완
+// i--와 --i 개념 때문에 헷갈렸던 문제
 
 #include <stdio.h>
 
-int str2int(char *pargv);
+int str2int(char *str);
 
-int main(int argc, char *argv[]) {
-	if (argc = 4) {
-		int num1 = str2int(argv[1]);
-		int num2 = str2int(argv[3]);
+int main(int argc, char **argv) {
+	
+	int num1, num2;
 
-		printf("%d", num1 + num2);
-	}
-	else {
+	if (argc != 4) {
 		printf("인자 수가 맞지 않습니다.\n");
 	}
+
+	num1 = str2int(argv[1]);
+	num2 = str2int(argv[3]);
+
+	printf("%d", num1 + num2);
+
 	return 0;
 }
 
-int str2int(char *pargv) {
+int str2int(char *str) {
 	int i = 0;
 
 	// 자리수 i 구하기
-	while (pargv[1][i] != '\0') {
+	while (str[i] != '\0') {
 		i++;
 	}
 
-	int j = i; // 현재 가리키는 수의 자리수
-	int sq = 1;
+	int times = 1; // 자리수에 따라 1, 10, 100을 곱해야하므로 그 수를 담는 변수
 	int converted = 0; // char에서 int로 바꾼 값
 
-	while (j-- >= 0) {
-		while (i - j > 0) {
-			sq *= 10;
-			j++;
-		}
-		converted += (pargv[1][i] - '0') * sq;
+	while (--i >= 0) {
+		converted += (str[i] - '0') * times;
+		times *= 10;
 	}
+
 	return converted;
 }
